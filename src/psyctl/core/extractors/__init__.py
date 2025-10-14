c4db27cb0d9b2b2f096aedda4c93959d9f1494f2
rick <rick@caveduck.io>
2025-10-14 21:59:27 +0900

    Enhance steering vector extraction by introducing Denoised Mean Difference method. Denoised Mean Difference, which utilizes PCA-based noise reduction for improved robustness.

--- src/psyctl/core/extractors/__init__.py
+++ src/psyctl/core/extractors/__init__.py
@@ -2,14 +2,16 @@
 
 from psyctl.core.extractors.base import BaseVectorExtractor
 from psyctl.core.extractors.bipo import BiPOVectorExtractor
+from psyctl.core.extractors.denoised_mean_difference import (
+    DenoisedMeanDifferenceVectorExtractor,
+)
 from psyctl.core.extractors.mean_difference import (
     MeanDifferenceActivationVectorExtractor,
 )
-from psyctl.core.extractors.pca_caa import PcaCaaExtractor
 
 __all__ = [
     "BaseVectorExtractor",
     "BiPOVectorExtractor",
+    "DenoisedMeanDifferenceVectorExtractor",
     "MeanDifferenceActivationVectorExtractor",
-    "PcaCaaExtractor",
 ]
